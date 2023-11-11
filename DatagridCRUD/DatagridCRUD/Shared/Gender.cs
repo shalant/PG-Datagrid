77efe2ea45972961a1435b6203aa6e5bdcccb019
shalant <doug.rosenberg@gmail.com>
2023-11-11 14:49:13 -0600

    added a 3rd tut

---
+++ DatagridCRUD/DatagridCRUD/Shared/Gender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatagridCRUD.Shared
+{
+    // This could totally just be 10,20,30!!!
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
